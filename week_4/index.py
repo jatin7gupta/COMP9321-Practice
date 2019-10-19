af714567cccce626ffef26a682a3850a33496fd4
jatin7gupta <guptajatin@outlook.com>
2019-10-20 07:16:06 +1100

    Update index.py
    Delete function implemented

--- week_4/index.py
+++ week_4/index.py
@@ -18,5 +18,10 @@ class Books(Resource):
         book = dict(df.loc[id])
         return book
 
+    def delete(self, id):
+        if id not in df.index:
+            api.abort(404, f"Book {id} does not exist")
+        df.drop(id, inplace=True)
+        return {"message": f"Book {id} is removed"}, 200
 
 app.run(debug=True)
