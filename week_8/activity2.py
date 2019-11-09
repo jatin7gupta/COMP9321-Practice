5a6c8c02b31a493e2bd22c96e66d228626da96de
jatin7gupta <guptajatin@outlook.com>
2019-11-10 08:00:02 +1100

    week 8 activity 2

--- week_8/activity2.py
+++ week_8/activity2.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import shuffle
+from sklearn.metrics import precision_score, accuracy_score, recall_score
 
 
 def load_iris(iris_path, split_percentage):
@@ -35,8 +37,7 @@ if __name__ == '__main__':
     # predict the test set
     predictions = knn.predict(iris_X_test)
 
-    print("Actual: ")
-    print(iris_y_test)
-
-    print("Predictions: ")
-    print(predictions)
+    print("confusion_matrix:\n", confusion_matrix(iris_y_test, predictions))
+    print("precision:\t", precision_score(iris_y_test, predictions, average=None))
+    print("recall:\t\t", recall_score(iris_y_test, predictions, average=None))
+    print("accuracy:\t", accuracy_score(iris_y_test, predictions))
